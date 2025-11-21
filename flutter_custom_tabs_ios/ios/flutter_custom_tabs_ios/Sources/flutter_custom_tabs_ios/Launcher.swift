8e099c3c72b7444a0ac33581a96e670810fc5b4f
Shinya Kumagai <roomful.rooms@gmail.com>
2025-11-21 22:52:39 +0900

    Apply code formatter to Launcher.swift

--- flutter_custom_tabs_ios/ios/flutter_custom_tabs_ios/Sources/flutter_custom_tabs_ios/Launcher.swift
+++ flutter_custom_tabs_ios/ios/flutter_custom_tabs_ios/Sources/flutter_custom_tabs_ios/Launcher.swift
@@ -79,11 +79,11 @@ private extension UIWindow {
     // iOS 13+
     if #available(iOS 13.0, *) {
       return UIApplication.shared.connectedScenes
-      .compactMap { $0 as? UIWindowScene }
-      .filter { $0.activationState == .foregroundActive }
-      .first?
-      .windows
-      .first { $0.isKeyWindow }
+        .compactMap { $0 as? UIWindowScene }
+        .filter { $0.activationState == .foregroundActive }
+        .first?
+        .windows
+        .first { $0.isKeyWindow }
     }
     // iOS 12 fallback
     return UIApplication.shared.windows.first { $0.isKeyWindow }
